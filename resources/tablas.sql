ab35d2c998a5b115461b6979ac6e3bfb35004da0
Kevin <kevinmb41@hotmail.com>
2019-02-12 12:29:03 +0100

    Ejercicio de ModelNew + funciones (done)

--- resources/tablas.sql
+++ resources/tablas.sql
@@ -7,3 +7,15 @@ create table noticias(
     fecha date not null,
     primary key(id)
 );
+
+
+-- Tabla Noticia
+drop table if exists noticia;
+
+create table noticia(
+    id int auto_increment,
+    titulo varchar(30) not null,
+    texto varchar (255) not null,
+    fecha date not null,
+    primary key(id)
+);
